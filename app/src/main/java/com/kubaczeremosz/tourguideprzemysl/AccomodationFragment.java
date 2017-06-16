309305f37186451b1483f5ab2afe262a745f8b57
glemigobles <sura.jacob@gmail.com>
2017-06-16 15:25:48 +0200

    skonczone

--- app/src/main/java/com/kubaczeremosz/tourguideprzemysl/AccomodationFragment.java
+++ app/src/main/java/com/kubaczeremosz/tourguideprzemysl/AccomodationFragment.java
@@ -17,9 +17,9 @@ public class AccomodationFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.accommodation, container, false);
 
         ArrayList<Offer> listAccommodation = new ArrayList<>();
-        listAccommodation.add(new Offer("Hotel Villa Bolestraszyce", getString(R.string.vbtext),R.drawable.vb,"49.8154662,22.8491607"));
-        listAccommodation.add(new Offer("Zamek dubiecko", getString(R.string.zdtext),R.drawable.zd,"49.8218965,22.3917756"));
-        listAccommodation.add(new Offer("Hotel Krasiczyn", getString(R.string.hktext),R.drawable.krasiczyn,"49.776357, 22.649975"));
+        listAccommodation.add(new Offer(getString(R.string.vbtitle), getString(R.string.vbtext),R.drawable.vb,"49.8154662,22.8491607"));
+        listAccommodation.add(new Offer(getString(R.string.zdtitle), getString(R.string.zdtext),R.drawable.zd,"49.8218965,22.3917756"));
+        listAccommodation.add(new Offer(getString(R.string.hktitle), getString(R.string.hktext),R.drawable.krasiczyn,"49.776357, 22.649975"));
 
 
         OfferAdapter offerAdapter=new OfferAdapter(getContext(), listAccommodation);
--- app/src/main/java/com/kubaczeremosz/tourguideprzemysl/FoodDrinksFragment.java
+++ app/src/main/java/com/kubaczeremosz/tourguideprzemysl/FoodDrinksFragment.java
@@ -20,12 +20,12 @@ public class FoodDrinksFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.food_drinks, container, false);
 
         ArrayList<Offer> listRestaurants = new ArrayList<>();
-        listRestaurants.add(new Offer("Bosko",getString(R.string.modkitch), R.drawable.bosko,"49.7830998,22.7752845,17"));
-        listRestaurants.add(new Offer("Hotel Krasiczyn",getString(R.string.tradkitch),R.drawable.krasiczyn,"49.776357, 22.649975"));
-        listRestaurants.add(new Offer("Hotel Villa Bolestraszyce",getString(R.string.tradkitch),R.drawable.vb,"49.8154662,22.8491607,17"));
-        listRestaurants.add(new Offer("Monarchia",getString(R.string.galitkitch),R.drawable.monarchia,"49.778489, 22.783430"));
-        listRestaurants.add(new Offer("Na górce",getString(R.string.fuskitch),R.drawable.nagorce,"49.806883, 22.763987"));
-        listRestaurants.add(new Offer("Zamek Dubiecko",getString(R.string.tradkitch),R.drawable.zd,"49.8218965,22.3917756,17"));
+        listRestaurants.add(new Offer(getString(R.string.boskotitle),getString(R.string.modkitch), R.drawable.bosko,"49.7830998,22.7752845,17"));
+        listRestaurants.add(new Offer(getString(R.string.hktitle),getString(R.string.tradkitch),R.drawable.krasiczyn,"49.776357, 22.649975"));
+        listRestaurants.add(new Offer(getString(R.string.vbtitle),getString(R.string.tradkitch),R.drawable.vb,"49.8154662,22.8491607,17"));
+        listRestaurants.add(new Offer(getString(R.string.monarchiatitle),getString(R.string.galitkitch),R.drawable.monarchia,"49.778489, 22.783430"));
+        listRestaurants.add(new Offer(getString(R.string.nagorcetitle),getString(R.string.fuskitch),R.drawable.nagorce,"49.806883, 22.763987"));
+        listRestaurants.add(new Offer(getString(R.string.zdtitle),getString(R.string.tradkitch),R.drawable.zd,"49.8218965,22.3917756,17"));
 
         OfferAdapter offerAdapter=new OfferAdapter(getContext(), listRestaurants);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_food_drinks);
